5166ea5939872df137feb65a7b183e09e04e80b5
Lacey Trokey <trokeyln@gmail.com>
2024-01-18 10:35:20 -0700

    added routes for each section, set up nav links, and tested

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+// Routes
+import App from './App.jsx';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutMe />,
+      },
+      {
+        path: 'portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'resume',
+        element: <Resume />,
+      },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
