bd898fd9bbdee174a276a460955bd905e4c0ed55
Lacey Trokey <trokeyln@gmail.com>
2024-01-18 22:12:46 -0700

    added about me description and bootstrap carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
 import { Outlet } from 'react-router-dom';
 import Header from './components/UI/Header';
 import Nav from './components/Nav';
 import Footer from './components/UI/Footer';
-
-
 
 function App() {
   return (
