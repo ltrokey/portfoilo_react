5ca3cb3429112e498280d91fca860660ea261a4a
Lacey Trokey <trokeyln@gmail.com>
2024-01-19 16:31:36 -0700

    added active class to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,37 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Navbar from "./UI/Navbar";
 
 export default function Nav() {
   return (
     <Navbar
       links={[
-        <Link key={1} className="nav-link" to="/">
+        <NavLink key={1} className="nav-link" activeClassName="active" to="/">
           About Me
-        </Link>,
-        <Link key={2} className="nav-link" to="/portfolio">
+        </NavLink>,
+        <NavLink
+          key={2}
+          className="nav-link"
+          activeClassName="active"
+          to="/portfolio"
+        >
           Portfolio
-        </Link>,
-        <Link key={3} className="nav-link" to="/contact">
+        </NavLink>,
+        <NavLink
+          key={3}
+          className="nav-link"
+          activeClassName="active"
+          to="/contact"
+        >
           Contact
-        </Link>,
-        <Link key={4} className="nav-link" to="/resume">
+        </NavLink>,
+        <NavLink
+          key={4}
+          className="nav-link"
+          activeClassName="active"
+          to="/resume"
+        >
           Resume
-        </Link>,
+        </NavLink>,
       ]}
     />
   );
