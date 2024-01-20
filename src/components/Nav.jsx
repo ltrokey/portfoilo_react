b275cc047c378e2df0fbd3b41fa1f87bfff704ca
Lacey Trokey <trokeyln@gmail.com>
2024-01-20 10:01:45 -0700

    fixed bug at active class to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,31 +5,16 @@ export default function Nav() {
   return (
     <Navbar
       links={[
-        <NavLink key={1} className="nav-link" activeClassName="active" to="/">
+        <NavLink key={1} className="nav-link" to="/">
           About Me
         </NavLink>,
-        <NavLink
-          key={2}
-          className="nav-link"
-          activeClassName="active"
-          to="/portfolio"
-        >
+        <NavLink key={2} className="nav-link" to="/portfolio">
           Portfolio
         </NavLink>,
-        <NavLink
-          key={3}
-          className="nav-link"
-          activeClassName="active"
-          to="/contact"
-        >
+        <NavLink key={3} className="nav-link" to="/contact">
           Contact
         </NavLink>,
-        <NavLink
-          key={4}
-          className="nav-link"
-          activeClassName="active"
-          to="/resume"
-        >
+        <NavLink key={4} className="nav-link" to="/resume">
           Resume
         </NavLink>,
       ]}
