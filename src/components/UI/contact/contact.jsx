bb5e07ead7def3556a76ac6e6131d76e7cc4fc0f
Lacey Trokey <trokeyln@gmail.com>
2024-01-22 11:38:23 -0700

    added styling to form button hover and renamed send

--- src/components/UI/contact/contact.jsx
+++ src/components/UI/contact/contact.jsx
@@ -106,7 +106,7 @@ export default function ContactForm() {
       </Form.Group>
 
       <Button variant="primary" type="submit">
-        Submit
+        Send
       </Button>
 
       {success && (
