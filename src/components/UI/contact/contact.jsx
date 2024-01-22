25934cf06c6a2c98049f3950bd1c71cd1c5a179f
Lacey Trokey <trokeyln@gmail.com>
2024-01-22 10:14:28 -0700

    fixing bug with Netlify Contact Form 04

--- src/components/UI/contact/contact.jsx
+++ src/components/UI/contact/contact.jsx
@@ -69,7 +69,7 @@ export default function ContactForm() {
       data-netlify-honeypot="bot-field"
     >
       {/* Added for Netlify */}
-      <input type="hidden" name="form-name" value="contact" />
+      <input type="hidden" name="form-name" value="contact-form" />
 
       <Form.Group controlId="formName">
         <Form.Label>Name</Form.Label>
@@ -113,7 +113,7 @@ export default function ContactForm() {
       </Form.Group>
 
       <Button variant="primary" type="submit">
-        Submit
+        Send
       </Button>
 
       {success && (
